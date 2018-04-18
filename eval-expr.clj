9010a97aa7f33fc7acf85713db00ae5f9242f3b6
Brian Maddy <brian@brianmaddy.com>
2018-04-18 05:02:57 -0500

    reformatting

--- eval-expr.clj
+++ eval-expr.clj
@@ -1,37 +1,34 @@
-  (defn eval-expr
-    [expr env]
-    ;; (prn expr)
-    ;; (prn [expr (symbol? expr) 'fn (symbol? 'fn)])
-    ;; (prn (get env expr))
-    (cond
-        (symbol? expr) (do (prn :get)
-                           (env expr))
-        (= 'fn (first expr)) (do (let [[_ [x] body] expr]
-                                   (prn [:fn x body])
-                                   (fn [arg]
-                                     (eval-expr body (fn [y]
-                                                       (prn {:x x :y y})
-                                                       (if (= x y)
-                                                         arg
-                                                         (get env y)))))))
-        :else (do (prn :else)
-                  (let [[rator rand] expr]
-                    ((eval-expr rator env)
-                     (eval-expr rand env))))))
+(defn eval-expr
+  [expr env]
+  (cond
+    (symbol? expr) (do (prn :get)
+                       (env expr))
+    (= 'fn (first expr)) (do (let [[_ [x] body] expr]
+                               (prn [:fn x body])
+                               (fn [arg]
+                                 (eval-expr body (fn [y]
+                                                   (prn {:x x :y y})
+                                                   (if (= x y)
+                                                     arg
+                                                     (get env y)))))))
+    :else (do (prn :else)
+              (let [[rator rand] expr]
+                ((eval-expr rator env)
+                 (eval-expr rand env))))))
 
-  (defn environment [y]
-    (throw (Exception. "oops")))
+(defn environment [y]
+  (throw (Exception. "oops")))
 
-  (eval-expr 'hello
-             (fn [arg]
-               (prn [:arg arg])
-               (if (= arg 'hello)
-                 'hello
-                 (environment arg))))
+(eval-expr 'hello
+           (fn [arg]
+             (prn [:arg arg])
+             (if (= arg 'hello)
+               'hello
+               (environment arg))))
 
-  (eval-expr '((fn (n) n) hello)
-             (fn [arg]
-               (if (= arg 'hello)
-                 'hello
-                 (environment arg))))
+(eval-expr '((fn (n) n) hello)
+           (fn [arg]
+             (if (= arg 'hello)
+               'hello
+               (environment arg))))
 
